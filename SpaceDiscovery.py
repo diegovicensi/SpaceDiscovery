e0dfa27cdfc8c242c593cd7436c7a4ace3b0fa74
Henrique Zanfir <1135519@atitus.edu.br>
2023-11-21 20:59:42 -0300

    nome da estrela na tela após clicar v1.0

--- SpaceDiscovery.py
+++ SpaceDiscovery.py
@@ -5,10 +5,13 @@ tamanho = (1000,563) #tupla
 branco = (255,255,255)
 preto = (0,0,0)
 pygame.display.set_caption("Space Marker")
+fonte = pygame.font.Font("freesansbold.ttf", 20)
 tela = pygame.display.set_mode ( tamanho )
 gameIcon = pygame.image.load("spacee.png")
 pygame.display.set_icon(gameIcon)
 fundo = pygame.image.load("bg.jpg")
+estrelas ={}
+pygame.display.update()
 rodando = True
 while rodando:
     for evento in pygame.event.get():
@@ -18,6 +21,10 @@ while rodando:
             pos = pygame.mouse.get_pos()
             item = simpledialog.askstring("Space", "Nome da Estrela:")
             print(item)
+            estrela = fonte.render(item, True, branco)
+            tela.blit(estrela, (pos))
+            pygame.draw.circle(tela, branco, pos, 20)
+            pygame.display.update()
             if item == None:
                 item = "desconhecido"+str(pos)
             estrelas[item] = pos
